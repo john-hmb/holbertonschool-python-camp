59a21360a3a4b9907ceb7bff799122b5c3dbd2c8
john hoffman <john-hmb@hoffmanhouse.us>
2018-06-02 22:42:01 +0000

    edit file

--- 0x01-python_hello_world/6-print_last_letter.py
+++ 0x01-python_hello_world/6-print_last_letter.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 my_word = "Holberton"
-print ("{:^3}is the last letter of the word".format(my_word[-1]))
+print ("{} is the last letter of the word {}".format(my_word[-1], my_word))
+
